a76f75d7a261c0789c9fc52bf6082dfd8d8f5b9c
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-07-03 00:27:04 +0300

    chapter5. understanding hashmap

--- src/main/java/edu/algorithms/grokkingalgorithms/chapter5/hashtable/SimplifiedTenBasedHashMap.java
+++ src/main/java/edu/algorithms/grokkingalgorithms/chapter5/hashtable/SimplifiedTenBasedHashMap.java
@@ -3,16 +3,27 @@ package edu.algorithms.grokkingalgorithms.chapter5.hashtable;
 import edu.algorithms.grokkingalgorithms.chapter5.hashtable.hashcodebased.TenBased;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class SimplifiedTenBasedHashMap<T extends TenBased> {
-    private T item;
-    int[] keysArray = new int[10];
-    int[] valuesArray = new int[10];
 
-    List<T> items = new LinkedList<>();
+    private LinkedList<T>[] valuesArray = new LinkedList[10];
 
     public void put(T item){
         int position = item.hashCode();
+        valuesArray[position].add(item);
     }
+
+    public boolean contains(T item){
+        int position = item.hashCode();
+        return valuesArray[position].contains(item);
+    }
+
+    public T get(int hash){
+        int position = hash % 10;
+        return valuesArray[position].get(0);
+    }
+
+
 }
+
+
---
+++ src/main/java/edu/algorithms/grokkingalgorithms/chapter5/hashtableimroved/HashTableImprovedExample.java
@@ -0,0 +1,6 @@
+package edu.algorithms.grokkingalgorithms.chapter5.hashtableimroved;
+
+public class HashTableImprovedExample<K,V> {
+    int[] keys = new int[50];
+
+}
