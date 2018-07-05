4732cda20871ea11676a6d632b68981adeb90aa8
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-07-05 19:00:32 +0300

    chapter 5. implementing HashMap

--- src/main/java/edu/algorithms/grokkingalgorithms/chapter5/hashtableimroved/HashTableImprovedExample.java
+++ src/main/java/edu/algorithms/grokkingalgorithms/chapter5/hashtableimroved/HashTableImprovedExample.java
@@ -1,6 +1,35 @@
 package edu.algorithms.grokkingalgorithms.chapter5.hashtableimroved;
 
-public class HashTableImprovedExample<K,V> {
-    int[] keys = new int[50];
+import java.util.HashMap;
 
+class Node<K,V> {
+    final int hash;
+    final K key;
+    V value;
+    Node<K,V> next;
+
+    public Node(int hash, K key, V value,
+            Node<K, V> next) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+}
+public class HashTableImprovedExample<K,V> {
+//    HashMap
+//String
+    Node<K,V>[] keys;
+
+//    public HashTableImprovedExample() {
+//        keys = new Node<K, V>[50];
+//    }
+
+    public static void main(String[] args) {
+        System.out.println("abc".hashCode());//96354
+        System.out.println("qwe".hashCode());//112383
+        final HashMap<String, String> stringStringHashMap = new HashMap<>();
+        stringStringHashMap.put("abc", "qwe");
+
+    }
 }
