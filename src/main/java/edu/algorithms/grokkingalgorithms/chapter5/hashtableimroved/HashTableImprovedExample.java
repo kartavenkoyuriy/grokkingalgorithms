82581636c738214c3285102af3df8bdfbd0350f7
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-07-10 06:29:30 +0300

    chapter5. finishhing hashmap

--- src/main/java/edu/algorithms/grokkingalgorithms/chapter5/hashtableimroved/HashTableImprovedExample.java
+++ src/main/java/edu/algorithms/grokkingalgorithms/chapter5/hashtableimroved/HashTableImprovedExample.java
@@ -12,7 +12,7 @@ class Node<K, V> {
 
     //TODO implement getters/setters
     public Node(int hash, K key, V value,
-            Node<K, V> next) {
+                Node<K, V> next) {
         this.hash = hash;
         this.key = key;
         this.value = value;
@@ -32,38 +32,42 @@ public class HashTableImprovedExample<K, V> {
 
 
     @SuppressWarnings("unchecked")
-    Node<K, V>[] keys = (Node<K, V>[]) new Node[10];
+    Node<K, V>[] nodes = (Node<K, V>[]) new Node[10];
 
     public V put(K key, V value) {
         final int hash = hash(key);
 
-        if (keys[hash] == null) {
-            @SuppressWarnings({"unchecked", "rawtypes"})
-            Node<K, V> createdNode = (Node<K, V>) new Node(hash, key, value, null);
-            keys[hash] = createdNode;
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        Node<K, V> createdNode = (Node<K, V>) new Node(hash, key, value, null);
+
+        if (nodes[hash] == null) {
+            nodes[hash] = createdNode;
             return null;
         } else {
-            Node<K, V> fetchedNode = keys[hash];
+            Node<K, V> fetchedNode = nodes[hash];
             while (fetchedNode != null) {
                 if (fetchedNode.key.equals(key)) {
                     V previousValue = fetchedNode.value;
                     fetchedNode.value = value;
                     return previousValue;
+                }
+
+                if (fetchedNode.next == null) {
+                    fetchedNode.next = createdNode;
+                    return null;
                 } else {
                     fetchedNode = fetchedNode.next;
                 }
+
             }
-            @SuppressWarnings({"unchecked", "rawtypes"})
-            Node<K, V> createdNode = (Node<K, V>) new Node(hash, key, value, null);
-            keys[hash] = createdNode;
-            return null;
         }
+        return null;
     }
 
     //TODO null checking
     public V get(K key) {
         final int hash = hash(key);
-        Node<K, V> fetchedNode = keys[hash];
+        Node<K, V> fetchedNode = nodes[hash];
         if (fetchedNode.value == null) {
             return null;
         } else {
@@ -83,16 +87,39 @@ public class HashTableImprovedExample<K, V> {
 
     @Override
     public String toString() {
+
+        StringBuilder arrayToString = new StringBuilder();
+        arrayToString.append("{");
+        for (int i = 0; i < nodes.length; i++) {
+            if (nodes[i] == null) {
+                arrayToString.append("null").append(",");
+            } else {
+                arrayToString.append("|");
+                Node<K, V> node = nodes[i];
+                while (node != null) {
+                    arrayToString.append(node).append(",");
+                    node = node.next;
+                }
+                arrayToString.append("|");
+            }
+        }
+        arrayToString.append("}");
+
         return "HashTableImprovedExample{" +
-                "keys=" + Arrays.toString(keys) +
+                "nodes=" + arrayToString.toString() +
                 '}';
     }
 
     public static void main(String[] args) {
         HashTableImprovedExample<String, String> map = new HashTableImprovedExample<>();
+        System.out.println("asd".hashCode());
+        System.out.println("asn".hashCode());
         map.put("asd", "1");
         map.put("asd", "2");
+        map.put("asn", "3");
         System.out.println(map);
+        System.out.println(map.get("asd"));
+        System.out.println(map.get("asn"));
 
     }
 
