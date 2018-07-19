8730970e82f30447be973b6c1ccfd70e35fcf781
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2018-07-19 23:17:28 +0300

    chapter7. started implemented dijkstra algorithm

---
+++ src/main/java/edu/algorithms/grokkingalgorithms/chapter7/DijkstraExample.java
@@ -0,0 +1,18 @@
+package edu.algorithms.grokkingalgorithms.chapter7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DijkstraExample {
+
+    Map<String, Map<String, Integer>> graph = new HashMap<>();
+
+    public String findTheShortest(Map<String, Map<String, Integer>> initialGraph){
+        //TODO may need to redo
+        int infinity = Integer.MAX_VALUE;
+
+
+        return null;
+    }
+
+}
